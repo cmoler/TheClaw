84bcee31773d5007b68e38e4aacc35b9f5de6ef0
coltonm <Colton_Moler@ultimatesoftware.com>
2019-07-02 14:11:11 -0400

    adding in java serial comm example

--- Java/TheClaw/src/Main.java
+++ Java/TheClaw/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SerialCommunication serialCommunication = new SerialCommunication();
+        serialCommunication.initialize();
+        Thread t = new Thread(){
+          public void run() {
+              try { Thread.sleep(1000000); } catch (InterruptedException ie) {}
+          }
+        };
+
+        t.start();
+        System.out.println("Started");
     }
 }
